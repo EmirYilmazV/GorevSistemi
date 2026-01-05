3b36e9373f335fdf41e4f0632ed50a94ff671dc1
EmirYilmazV <emiryilmaz2505@gmail.com>
2026-01-06 00:05:54 +0300

    auth.php

--- auth.php
+++ auth.php
@@ -1,7 +1,15 @@
 <?php
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
+
+
+if (!isset($_SESSION['user_id']) && isset($_COOKIE['user_id']) && isset($_COOKIE['role'])) {
+    $_SESSION['user_id'] = $_COOKIE['user_id'];
+    $_SESSION['role'] = $_COOKIE['role'];
+}
+
 
 if (!isset($_SESSION['user_id']) || !isset($_SESSION['role'])) {
     header("Location: index.php");
