731282307baedbf903add1027dfc4905dad36b87
EmirYilmazV <emiryilmaz2505@gmail.com>
2026-01-06 00:04:47 +0300

    login_patron.php

--- login_patron.php
+++ login_patron.php
@@ -1,9 +1,13 @@
 <?php
+session_start(); 
+
 include "db.php";
 
 $error = "";
 
+
 if ($_POST) {
+
     $stmt = $pdo->prepare(
         "SELECT * FROM users WHERE username=? AND password=? AND role='patron'"
     );
@@ -11,25 +15,45 @@ if ($_POST) {
     $user = $stmt->fetch();
 
     if ($user) {
+
+     
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['role'] = 'patron';
+
+      
+        if (isset($_POST['remember'])) {
+            setcookie("user_id", $user['id'], time() + 604800, "/");
+            setcookie("role", "patron", time() + 604800, "/");
+        }
+
         header("Location: patron.php");
         exit;
+
     } else {
         $error = "Patron bilgileri hatalı";
     }
 }
 ?>
 
+
 <link rel="stylesheet" href="style.css">
 
 <div class="container">
     <h2>Patron Girişi</h2>
-    <?php if ($error): ?><div class="error"><?= $error ?></div><?php endif; ?>
+
+    <?php if ($error): ?>
+        <div class="error"><?= $error ?></div>
+    <?php endif; ?>
 
     <form method="post">
-        <input name="username" placeholder="Kullanıcı adı">
-        <input type="password" name="password" placeholder="Şifre">
-        <button>Giriş Yap</button>
-    </form>
+    <input name="username">
+    <input type="password" name="password">
+
+    <div class="remember">
+        <input type="checkbox" name="remember" value="1">
+        <label>Beni hatırla</label>
+    </div>
+
+    <button>Giriş Yap</button>
+</form>
 </div>
